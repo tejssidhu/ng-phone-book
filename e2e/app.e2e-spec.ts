c4af0939b5f6bb7ef845e16fce8adc0f06e7548d
Tej Sidhu <tej.sidhu@ivanti.com>
2018-03-16 16:29:09 +0000

    initial e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('ng-phone-book App', () => {
+describe('ngPhoneBook App Login Page', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display header message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getHeader()).toEqual('Phonebook is here!');
   });
 });
