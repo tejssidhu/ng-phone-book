d9655a8074b89f642af24c813e352797bbba81c0
Tej Sidhu <tej.sidhu@ivanti.com>
2018-03-16 09:43:10 +0000

    Add junit reporter so test results can be published to VSTS

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
@@ -23,7 +24,11 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReport: {
+      outputDir: '', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'test.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+    },
+    reporters: ['progress', 'kjhtml','junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
